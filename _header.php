97a34a91e2e1d4b718f5b996c2ca20440bd2c986
ahidayati <hidayati.ann@gmail.com>
2022-01-14 16:25:48 +0100

    docs : add README

--- _header.php
+++ _header.php
@@ -6,8 +6,11 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Book Store Database</title>
+    <link rel="icon" href="https://img.icons8.com/external-kiranshastry-gradient-kiranshastry/64/000000/external-book-interface-kiranshastry-gradient-kiranshastry-1.png">
+
     <!--bootstrap stylesheet-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
     <!--css stylesheet-->
     <link rel="stylesheet" href="styles/styles.css">
     <!-- google fonts-->
--- class/PrintedItem.php
+++ class/PrintedItem.php
@@ -6,7 +6,7 @@ class PrintedItem extends Connection
     public function displayAllItem()
     {
         $pdo = $this->connect();
-        $query = "SELECT printed_item.id, title, author, `ean_isbn`,`publisher`,`published_date`, name_genre, name_topic, name_type, `stock`, `price` FROM `printed_item` JOIN `genre` ON genre.id = printed_item.genre_id JOIN `topic` ON topic.id = printed_item.topic_id JOIN `type` ON type.id = printed_item.type_id;";
+        $query = "SELECT printed_item.id, title, author, ean_isbn, publisher, published_date, name_genre, name_topic, name_type, stock, price FROM `printed_item` JOIN `genre` ON genre.id = printed_item.genre_id JOIN `topic` ON topic.id = printed_item.topic_id JOIN `type` ON type.id = printed_item.type_id;";
         $results = $pdo->query($query);
         $results->execute();
 
